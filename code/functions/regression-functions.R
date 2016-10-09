ac6d6e33fc053d184f5a102bd690ffd0a4d54a4e
Yuyu Zhang <zyyucla@gmail.com>
2016-10-09 13:00:18 -0700

    initializing all the functions

---
+++ code/functions/regression-functions.R
@@ -0,0 +1,19 @@
+residual_sum_squares<-function(x){
+
+ }
+
+total_sum_squares<-function(x){
+
+}
+
+r_squared<-function(x){
+
+}
+
+f_statistic<-function(x){
+
+}
+
+residual_std_error<-function(x){
+
+}
