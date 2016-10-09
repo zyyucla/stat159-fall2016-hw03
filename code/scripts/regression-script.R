b23368e530196aa3bed98172aa648402aa5bdfc8
Yuyu Zhang <zyyucla@gmail.com>
2016-10-09 09:43:45 -0700

    first few lines

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -1,1 +1,12 @@
-#T
+#Read data 
+dat<-read.csv("../data/Advertising.csv",row.names=1)
+
+#Linear regression between Sales and TV
+reg_tv<-lm(Sales~TV, data=dat)
+regsum_tv<-summary(reg_tv)
+names(regsum_tv)
+save(regsum_tv, file="../regression.RData")
+
+
+
+
