e2a9ec00a4bec04181c41700ccee6027b323b465
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 15:49:21 -0700

    add radio linear regression

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -8,5 +8,11 @@ names(regsum_tv)
 save(regsum_tv, file="../regression.RData")
 
 
+#Linear regression between Sales and Radio
+reg_radio<-lm(Sales~Radio,data=dat)
+regsum_radio<-summary(reg_radio)
+names(regsum_radio)
+save(regsum_radio,file="../regression.RData")
 
+#Linear regression between Sales and Newspaper
 
