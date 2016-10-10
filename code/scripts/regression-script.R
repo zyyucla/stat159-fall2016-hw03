c35db4fcfa67a364b83a0e8233deb1c228446117
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 16:03:20 -0700

    modifying regression file

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -4,15 +4,15 @@ dat<-read.csv("../../data/Advertising.csv",row.names=1)
 #Linear regression between Sales and TV
 reg_tv<-lm(Sales~TV, data=dat)
 regsum_tv<-summary(reg_tv)
-names(regsum_tv)
-save(regsum_tv, file="../regression.RData")
+names(reg_tv)
+save(reg_tv, file="../regression.RData")
 
 
 #Linear regression between Sales and Radio
 reg_radio<-lm(Sales~Radio,data=dat)
 regsum_radio<-summary(reg_radio)
-names(regsum_radio)
-save(regsum_radio,file="../regression.RData")
+names(reg_radio)
+save(reg_radio,file="../regression.RData")
 
 #Linear regression between Sales and Newspaper
 
