771469bb44de9b75156cb23276fbc354c7c88ed5
Yuyu Zhang <zyyucla@gmail.com>
2016-10-11 13:43:21 -0700

    testing file

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -6,6 +6,8 @@ mul_reg<-lm(Sales~TV+Radio+Newspaper, data=dat)
 mulsum_reg<-summary(mul_reg)
 names(mulsum_reg)
 save(mul_reg, file="../../data/regression.RData")
+png("../../images/test.png)
+plot(mul_reg, which=1:4)
 
 #TV-Sales scatterplot
 reg_tv<-lm(Sales~TV,data=dat)
