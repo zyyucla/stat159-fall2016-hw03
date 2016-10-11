2161c4403025cbe923f8e400bca42c3851c6195d
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 18:40:07 -0700

    adding scatterplot matrix

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -14,17 +14,21 @@ sink()
 save(cor(dat), "../../data/correlation-matrix.RData")
 
 #create histogram of all the variables
-png("../images/histogram-tv.png")
+png("../../images/histogram-tv.png")
 hist(dat$TV,main="Histogram of TV", xlab="TV")
 
-png("../images/histogram-radio.png")
+png("../../images/histogram-radio.png")
 hist(dat$Radio,main="Histogram of Radio", xlab="Radio")
 
-png("../images/histogram-newspaper.png")
+png("../../images/histogram-newspaper.png")
 hist(dat$Newspaper,main="Histogram of Newspaper", xlab="Newspaper")
 
-png("../images/histogram-sales.png")
+png("../../images/histogram-sales.png")
 hist(dat$Sales,main="Histogram of Sales", xlab="Sales")
+
+#create matrix scatterplot
+png("../../images/scatterplot-matrix.png")
+pairs(~Sales+TV+Newspaper+Radio, data=dat, main="Scatterplot Matrix of all variables")
 dev.off()
 
 
