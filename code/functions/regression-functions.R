3b6181c1a1be5ced64173729517766f003ac7f03
Yuyu Zhang <zyyucla@gmail.com>
2016-10-11 14:33:21 -0700

    adding description

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -1,19 +1,37 @@
+#description:computes the residual sum of squares of a "lm" object 
+#input:a "lm" object
+#output:RSS(numerical value)
+
 residual_sum_squares<-function(x){
 
  }
 
+
+#description:computes the total sum of squares of a "lm" object
+#input:a "lm" object
+#output:TSS (numerical value)
 total_sum_squares<-function(x){
 
 }
 
+#description:computes the coefficient of determination of a "lm" object
+#input:a "lm" object
+#output:R square(numerical value)
 r_squared<-function(x){
 
 }
 
+#description:computes the F-statistic of a "lm" object
+#input:a "lm" object
+#output: F-statistics(numerical value)
 f_statistic<-function(x){
 
 }
 
+
+#description:computes the residual standard error of a "lm" object
+#input:a "lm" object
+#output: RSE(numerical value)
 residual_std_error<-function(x){
 
 }
