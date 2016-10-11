e4a26a806e3a6eace589f46f4f2cf9d90c5093da
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 19:19:16 -0700

    fix typo

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -4,7 +4,7 @@ dat<-read.csv("../../data/Advertising.csv",row.names=1)
 #get the multiple linear regression and save the data
 mul_reg<-lm(Sales~TV+Radio+Newspaper, data=dat)
 mulsum_reg<-summary(mul_reg)
-names(mulsum)
+names(mulsum_reg)
 save(mul_reg, file="../../data/regression.RData")
 
 #TV-Sales scatterplot
