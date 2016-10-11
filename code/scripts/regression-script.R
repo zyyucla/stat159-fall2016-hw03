962bffb97cd3d2b844a220b399c271420f3cf154
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 19:18:03 -0700

    addingg scatterplot

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -7,6 +7,20 @@ mulsum_reg<-summary(mul_reg)
 names(mulsum)
 save(mul_reg, file="../../data/regression.RData")
 
+#TV-Sales scatterplot
+png("../../images/scatterplot-tv-sales.png")
+plot(dat$TV,dat$Sales,main="Linear regression of TV ads on Sales",xlab="TV", ylab="Sales",pch = 19,col="#8e8f94")
+abline(reg,col="#bd0300",lwd=2)
+
+#Radio-Sales scatterplot
+png("../../images/scatterplot-radio-sales.png")
+plot(dat$Radio,dat$Sales,main="Linear regression of Radio on Sales",xlab="Radio", ylab="Sales",pch = 19,col="#8e8f94")
+abline(reg,col="#bd0300",lwd=2)
+
+#Newspaper-Sales scatterplot
+png("../../images/scatterplot-newspaper-sales.png")
+plot(dat$Newspaper,dat$Sales,main="Linear regression of Newspaper on Sales",xlab="Newspaper", ylab="Sales",pch = 19,col="#8e8f94")
+abline(reg,col="#bd0300",lwd=2)
 
 
-
+#
