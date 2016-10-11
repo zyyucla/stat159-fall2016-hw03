4739123950e53c302c50875bfa0558b61389a401
Yuyu Zhang <zyyucla@gmail.com>
2016-10-10 18:27:55 -0700

    first test

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -1,18 +1,12 @@
 #Read data 
 dat<-read.csv("../../data/Advertising.csv",row.names=1)
 
-#Linear regression between Sales and TV
-reg_tv<-lm(Sales~TV, data=dat)
-regsum_tv<-summary(reg_tv)
-names(reg_tv)
-save(reg_tv, file="../regression.RData")
+#get the multiple linear regression and save the data
+mul_reg<-lm(Sales~TV+Radio+Newspaper, data=dat)
+mulsum_reg<-summary(mul_reg)
+names(mulsum)
+save(mul_reg, file="../../data/regression.RData")
 
 
-#Linear regression between Sales and Radio
-reg_radio<-lm(Sales~Radio,data=dat)
-regsum_radio<-summary(reg_radio)
-names(reg_radio)
-save(reg_radio,file="../regression.RData")
 
-#Linear regression between Sales and Newspaper
 
