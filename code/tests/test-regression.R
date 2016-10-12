e6c229a7d9dbfd448ce369e487beead64c42e1f0
Yuyu Zhang <zyyucla@gmail.com>
2016-10-11 20:34:34 -0700

    initializing the tests file

---
+++ code/tests/test-regression.R
@@ -0,0 +1,17 @@
+source("../functions/regression-functions.R")
+
+#test the residual_sum_squares function
+context("Test for different functions")
+test_that(
+
+
+#test the total_sum_squares function
+
+
+#test the r_square function
+
+
+#test the F-statistic function
+
+
+#test the residual_std_error function
