742091867ce7b7a838a288bfa4a16a25791a342b
Yuyu Zhang <zyyucla@gmail.com>
2016-10-12 10:08:34 -0700

    update all the functions

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -11,21 +11,23 @@ residual_sum_squares<-function(x){
 #input:a "lm" object
 #output:TSS (numerical value)
 total_sum_squares<-function(x){
-   
+   rss<-residual_sum_squares(x)
+   r2<summary(x)$r.squared
+   rss/(1-r2)
 }
 
 #description:computes the coefficient of determination of a "lm" object
 #input:a "lm" object
 #output:R square(numerical value)
 r_squared<-function(x){
-   
+   r2<summary(x)$r.squared
 }
 
 #description:computes the F-statistic of a "lm" object
 #input:a "lm" object
 #output: F-statistics(numerical value)
 f_statistic<-function(x){
-
+  summary(x)$fstatistic[1]
 }
 
 
@@ -33,5 +35,5 @@ f_statistic<-function(x){
 #input:a "lm" object
 #output: RSE(numerical value)
 residual_std_error<-function(x){
-
+    sqrt(deviance(x)/df.residual(x))
 }
