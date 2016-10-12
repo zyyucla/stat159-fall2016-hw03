0f39114fea0b7f558bea9a2465feb260a9ff7157
Yuyu Zhang <zyyucla@gmail.com>
2016-10-12 09:45:43 -0700

    writing the first function

--- code/functions/regression-functions.R
+++ code/functions/regression-functions.R
@@ -11,14 +11,14 @@ residual_sum_squares<-function(x){
 #input:a "lm" object
 #output:TSS (numerical value)
 total_sum_squares<-function(x){
-
+   
 }
 
 #description:computes the coefficient of determination of a "lm" object
 #input:a "lm" object
 #output:R square(numerical value)
 r_squared<-function(x){
-
+   
 }
 
 #description:computes the F-statistic of a "lm" object
