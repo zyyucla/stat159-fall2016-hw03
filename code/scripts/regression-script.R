8466bc4e519728db5b185edf6a205e59c0923cd8
Yuyu Zhang <zyyucla@gmail.com>
2016-10-12 10:02:55 -0700

    update the summary

--- code/scripts/regression-script.R
+++ code/scripts/regression-script.R
@@ -5,8 +5,7 @@ dat<-read.csv("../../data/Advertising.csv",row.names=1)
 mul_reg<-lm(Sales~TV+Radio+Newspaper, data=dat)
 mulsum_reg<-summary(mul_reg)
 names(mulsum_reg)
-names(mul_reg)
-save(mul_reg,file="../../data/regression.RData")
+save(mul_reg,mulsum_reg,file="../../data/regression.RData")
 
 
 #TV-Sales scatterplot
