9211bdbb9cd7685b7fe00bc1fe8a8be902b4906e
Yuyu Zhang <zyyucla@gmail.com>
2016-10-12 10:25:23 -0700

    update all the functions

--- code/tests/test-regression.R
+++ code/tests/test-regression.R
@@ -1,44 +1,45 @@
 source("../functions/regression-functions.R")
+context("Test for multiple regression functions")
+
+#Use built-in dataset mtcars as a test
+reg <- lm(mpg ~ disp + hp, data = mtcars)
+regsum <- summary(reg)
 
 
 #test the residual_sum_squares function
-context("Test for residual_sum_squares function")
-test_that("RSS for a given 'lm' object" {
-    expect_equal(residual_sum_squares(x), )
-    expect_length(residual_sum_squares(x),1)
-    expect_type(residual_sum_squares(x),'double')
+test_that("RSS function is working",{
+    expect_equal(residual_sum_squares(reg),sum(reg$residuals^2))
+    expect_length(length(residual_sum_squares(reg)),1)
+    expect_type(residual_sum_squares(reg),'double')
 })
 
 
 #test the total_sum_squares function
-context("Test for total_sum_squres function")
-test_that("TSS for a given 'lm' object" {
-    expect_equal(total_sum_squares(x),)
-    expect_length(total_sum_squares(x),1)
-    expect_type(total_sum_squares(x),'double')
+test_that("TSS function is working", {
+    expect_equal(total_sum_squares(reg), sum((mtcars$mpg - mean(mtcars$mpg))^2))
+    expect_length(length(total_sum_squares(reg)),1)
+    expect_type(total_sum_squares(reg),'double')
 
 })
 
 #test the r_square function
-context("Test for r_square function")
-test_that ("R^2 for a given 'lm' object" {
-    expect_equal(r_squared(),)
-    expect_length(r_squared(),1)
-    expect_type(r_squared(),'double')
+test_that ("R square function is working",{
+    expect_equal(r_squared(reg),regsum$r.squared)
+    expect_gt(r_squared(reg), 0)
+    expect_lt(r_squared(reg), 1)
+    expect_type(r_squared(reg),'double')
 })
 
 #test the F-statistic function
-context("Test for F-statistic function")
-test_that ("F-statistic for a given 'lm' object"{
-   expect_equal(f-statistic(x),)
-   expect_length(f-statistic(x),1)
-   expect_type(f-statistic(x),'double')
+test_that ("F-statistic function is working"{
+   expect_equal(f-statistic(reg),regsum$fstatistic[1])
+   expect_length(length(f-statistic(reg)),1)
+   expect_gt(f_statistic(reg), 0)
 })
 
 #test the residual_std_error function
-context("Test for residual_std_error function")
-test_that ("RSD for a given 'lm' object" {
-   expect_equal(residual_std_error(x),)
-   expect_length(residual_std_error(x),1)
-   expect_type(residual_std_error(x),'double')
+test_that ("RSD function is working" {
+   expect_equal(residual_std_error(reg), regsum$sigma)
+   expect_length(length(residual_std_error(reg)),1)
+   expect_type(residual_std_error(reg),'double')
 })
